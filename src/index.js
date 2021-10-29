19946d011a40e6efb327bfa25eea409372362caf
Amy Liu <amy.liu@cultureamp.com>
2021-10-29 15:26:14 +1100

    add fisrt componenet

--- src/index.js
+++ src/index.js
@@ -3,6 +3,10 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 // Create a react component
-
+// const App = function() {
+const App = () => { // ES2015
+  return <div>Hi there!</div>;
+}
 
 // Take the react component and show it on ther screen
+ReactDOM.render(<App />, document.querySelector('#root'));
